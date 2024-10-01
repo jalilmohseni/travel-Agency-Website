cb31061d7d8f4f2fdb94409578264d2797e77078
jalil1984 <jalil.mohseni@gmail.com>
2024-10-01 12:52:32 -0600

    lets submit it

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
 
-//-------------------------------------------------------------------------------------------------------------
-// JavaScript Logic:
-// **Goal:Add dynamic behavior and DOM manipulation as well as form Validate**
-// Steps:
-// 1.	Image Gallery Logic:
-//   -Create arrays for images, descriptions, and URL links (4 images).
-//   -Dynamically generate table rows (tr) based on array length and cells (<td>).
-//   -Add event listeners for each table cell to display image previews and open the URL link on click.
-// 2.  Image Window Opening & Closing:
-//   - on click @ each table rows a new window opens with cllickable links and gives the user 5 seconds to react. 
-//   - if user clicks the link in new window a website will open to provide more information about destination.
-//   - if user doesnt react within 5 seconds the new window will close automatically.
-// 3.	Form input logic and Validation:
-//   -Use JavaScript confirm() dialogs to ask users if they are sure about submitting or resetting the form.
-//   -Create functions to validate each form input (ensure that the name, address, etc., are not empty).
-//   -showing instructional text when inputs are focused and highlighting incorrect fields.
-//   -Prevent form submission if validation fails.
-
-//-------------------------------------------------------------------------------------------------------------------
-
-// Ensure the DOM is loaded before running the script
-//document.addEventListener('DOMContentLoaded', function() {
 //     // ** Image Gallery Logic **
 
     // Arrays for images, descriptions, and URLs
